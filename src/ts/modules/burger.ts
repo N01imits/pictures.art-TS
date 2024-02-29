853993e2a7c3698761e490cc21927e290d1d27c4
Ilya Belyakov <darksiders1282@gmail.com>
2024-02-29 16:19:23 +0300

    Refactor burger module to use a breakpoint constant

--- src/ts/modules/burger.ts
+++ src/ts/modules/burger.ts
@@ -1,17 +1,14 @@
 export const burger = (burgerSelector: string, menuSelector: string) => {
 	const burgerMenu: HTMLUListElement | null = document.querySelector(menuSelector);
 	const burgerButton: HTMLButtonElement | null = document.querySelector(burgerSelector);
-	const screenWidthThreshold = 992;
+	const breakpoint = 992;
 
 	if (!burgerMenu || !burgerButton) return;
 
 	burgerMenu.style.display = 'none';
 
 	burgerButton.addEventListener('click', () => {
-		if (
-			burgerMenu.style.display === 'none' &&
-			window.screen.availWidth <= screenWidthThreshold
-		) {
+		if (burgerMenu.style.display === 'none' && window.screen.availWidth <= breakpoint) {
 			burgerMenu.style.display = 'block';
 		} else {
 			burgerMenu.style.display = 'none';
@@ -19,7 +16,7 @@ export const burger = (burgerSelector: string, menuSelector: string) => {
 	});
 
 	window.addEventListener('resize', () => {
-		if (window.screen.availWidth > screenWidthThreshold) {
+		if (window.screen.availWidth > breakpoint) {
 			burgerMenu.style.display = 'none';
 		}
 	});
