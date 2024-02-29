4225e9911cbb31c28be6b3b6f119229cc2764dc2
Ilya Belyakov <darksiders1282@gmail.com>
2024-02-29 14:09:00 +0300

    Refactored burger menu logic

--- src/ts/modules/burger.ts
+++ src/ts/modules/burger.ts
@@ -1,13 +1,17 @@
 export const burger = (burgerSelector: string, menuSelector: string) => {
 	const burgerMenu: HTMLUListElement | null = document.querySelector(menuSelector);
 	const burgerButton: HTMLButtonElement | null = document.querySelector(burgerSelector);
+	const screenWidthThreshold = 992;
 
 	if (!burgerMenu || !burgerButton) return;
 
 	burgerMenu.style.display = 'none';
 
 	burgerButton.addEventListener('click', () => {
-		if (burgerMenu.style.display === 'none' && window.screen.availWidth < 993) {
+		if (
+			burgerMenu.style.display === 'none' &&
+			window.screen.availWidth <= screenWidthThreshold
+		) {
 			burgerMenu.style.display = 'block';
 		} else {
 			burgerMenu.style.display = 'none';
@@ -15,7 +19,7 @@ export const burger = (burgerSelector: string, menuSelector: string) => {
 	});
 
 	window.addEventListener('resize', () => {
-		if (window.screen.availWidth > 992) {
+		if (window.screen.availWidth > screenWidthThreshold) {
 			burgerMenu.style.display = 'none';
 		}
 	});
